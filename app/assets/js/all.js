f5ea30c9e759cddd02585cd7c0fa084eddd010f9
Sin <102574135+yusin07@users.noreply.github.com>
2022-09-06 20:24:53 +0800

    0906 update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,22 @@
-$(function() {
+$(function () {
   console.log('Hello Bootstrap5');
+});
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 4000,
+  },
+  centeredSlides: true,
+  slidesPerView: "auto",
+  breakpoints: {
+    992: {
+      slidesPerView: 3,
+    }
+  }
 });
